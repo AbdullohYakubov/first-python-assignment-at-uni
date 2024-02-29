2847d657791fae88a5801327079c5047a90412b1
Abdullox <yakubovabdulloh10@gmail.com>
2024-02-29 18:02:51 +0500

    finished the sorting algorithm and made the code clearer

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ user_input_list = user_input.split(',')
 
 valid_chars = '0123456789.'
 input_index = 0
+is_valid = True
 
 for input in user_input_list:
     decimal_point_count = 0
@@ -12,59 +13,68 @@ for input in user_input_list:
             if element == '.':
                 decimal_point_count += 1
                 if decimal_point_count > 1:
+                    is_valid = False
                     print('Invalid Input')
                     break
         else:
-            # print('Invalid characters')
+            is_valid = False
+            print('Error: Invalid characters!')
             break
     else:
         pass
-        # print('Valid float')
-        # element = float(element)
-        # print(isinstance(element, float))
 
-    # input = float(input)
-    # print(isinstance(input, float))
+    if is_valid and (not input == ''):
+        user_input_list[input_index] = float(input)
+        input_index += 1
 
-    user_input_list[input_index] = float(input)
-    input_index += 1
-
-print(user_input_list)
+if is_valid:
+    print(f'The converted list: {user_input_list}')
 
 # Calculating the mean
 running_total = 0
 for i in range(len(user_input_list)):
-    running_total += user_input_list[i]
+    if is_valid and (not user_input_list[i] == ''):
+        running_total += user_input_list[i]
+    else:
+        break
 mean_value = running_total / len(user_input_list)
-print(mean_value)
+
+if is_valid:
+    print(f'The mean: {mean_value}')
 
 # Calculating the mode
 number_of_instances = 1
 mode = 0
 for i in range(len(user_input_list)):
-    # number_of_instances += user_input_list.count(i) - number_of_instances 
     if user_input_list.count(user_input_list[i]) > number_of_instances:
         number_of_instances = user_input_list.count(user_input_list[i])
         mode = user_input_list[i]  
-print(mode) 
+    else:
+        mode = user_input_list[0]
+if is_valid:
+    print(f'The mode: {mode}') 
 
 # Calculating the standard deviation
 mean_deviations = 0
 for i in range(len(user_input_list)):
-    mean_deviations += pow(user_input_list[i] - mean_value, 2)
+    if is_valid and (not user_input_list[i] == ''):
+        mean_deviations += pow(user_input_list[i] - mean_value, 2)
+    else:
+        break
 standard_deviation_value = pow(mean_deviations / len(user_input_list), 1/2)
-print(standard_deviation_value)
+
+if is_valid:
+    print(f'The standard deviation: {standard_deviation_value}')
 
 # Sorting
-# first_number = 0
-# second_number = 0
-# new_arr = []
-for i in range(len(user_input_list)):
-    if user_input_list[i] > user_input_list[i+1]:
-        if user_input_list[i] != user_input_list[len(user_input_list) - 1]:
-            # user_input_list = [user_input_list[i+1], user_input_list[i]]
-            user_input_list.append(user_input_list[i+1])
-            user_input_list.append(user_input_list[i])
-        else:
-            continue
-print(user_input_list)
+index = 0
+for i in range(0, len(user_input_list)):
+    if is_valid and (not user_input_list[i] == ''):
+        for j in range(i + 1, len(user_input_list)):
+            if user_input_list[i] >= user_input_list[j]:
+                user_input_list[i], user_input_list[j] = user_input_list[j], user_input_list[i]
+    else:
+        break
+
+if is_valid:
+    print(f'The sorted list: {user_input_list}')
